4d8fa7b4f00290536bbd6d5b932df692e04db8ec
Mujahida <mujahidaafridi0307@gmail.com>
2021-05-31 12:41:00 +0500

    well

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text("Login Form")),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: 200,
+                  child: TextField(),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 200,
+                  child: TextField(),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Login"))
+              ],
+            ),
+          ),
+        ));
+  }
+}
